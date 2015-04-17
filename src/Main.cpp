92b193b1feea14033ccc2b684e539c4028e0e825
Andreas Schoepfer <andreas.schoepfer@erni.ch>
2015-04-17 14:44:34 +0200

    new constructors with parent node ptr instead of parent path, test updated

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,26 +27,26 @@ int main()
 
 
   printf("*CREATE ROOT NODE*\n");
-  DbgCli_Node* rootNode = new DbgCli_Topic("", "mmi", "MMI node.");
-  DbgCli_Node::AssignRootNode(rootNode);
+  DbgCli_Topic* rootNode = new DbgCli_Topic(0, "mmi", "MMI node.");
+  DbgCli_Node::AssignRootNode(static_cast<DbgCli_Node*>(rootNode));
   printf("MMI Node: %s\n", DbgCli_Node::RootNode()->getNodeName());
   printf("MMI Helptext: %s\n", DbgCli_Node::RootNode()->getHelpText());
 
   printf("\n*ADD TOPIC*\n");
-  DbgCli_Node* screenNode = new DbgCli_Topic("mmi", "screen", "Screen Node.");
+  DbgCli_Topic* screenNode = new DbgCli_Topic(rootNode, "screen", "Screen Node.");
   printf("Screen Node: %s\n", screenNode->getNodeName());
   printf("Screen Helptext: %s\n", screenNode->getHelpText());
 
   printf("\n*ADD COMMAND*\n");
-  DbgCli_Node* showCmd = new DbgCli_Command("mmi screen", "show", "Show Node, shows current screen.");
+  DbgCli_Node* showCmd = new DbgCli_Command(screenNode, "show", "Show Node, shows current screen.");
   printf("MMI Screen Show Node: %s\n", showCmd->getNodeName());
   printf("Show Helptext: %s\n", showCmd->getHelpText());
 
   printf("\n*ADD OTHER BRANCH*\n");
-  new DbgCli_Topic("mmi", "sensorA", "This is Sensor A");
-  new DbgCli_Topic("mmi sensorA", "subX", "subsystem x of sensor A");
-  DbgCli_Node* topicY = new DbgCli_Topic("mmi sensorA subX", "topicY", "TopicY in subsystemX of sensor A");
-  new DbgCli_Command("mmi sensorA subX", "topicZ", "TopicZ in subsystemX of sensor A");
+  DbgCli_Topic* sensorA = new DbgCli_Topic(rootNode, "sensorA", "This is Sensor A");
+  DbgCli_Topic* subX = new DbgCli_Topic(sensorA, "subX", "subsystem x of sensor A");
+  DbgCli_Topic* topicY = new DbgCli_Topic(subX, "topicY", "TopicY in subsystemX of sensor A");
+  new DbgCli_Command(subX, "commandZ", "CommandZ in subsystemX of sensor A");
   printf("TopicY Node: %s\n", topicY->getNodeName());
   printf("TopicY Helptext: %s\n", topicY->getHelpText());
   printf("----\n");
@@ -65,32 +65,32 @@ int main()
   const char* args_mmi_screen_show[] = {"mmi", "screen", "show"};
   unsigned int argc = 3;
   printf("\nAccess all nodes\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_mmi_screen_show, 1);
+  DbgCli_Node::RootNode()->execute(argc, args_mmi_screen_show, 1);
 
   const char* args_mmi_screen[] = {"mmi", "screen"};
   argc = 2;
   printf("\nAccess node in the middle\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_mmi_screen, 1);
+  DbgCli_Node::RootNode()->execute(argc, args_mmi_screen, 1);
 
   const char* args_mmi[] = {"mmim"};
   argc = 1;
   printf("\nAccess invalid root node\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_mmi, 0);
+  DbgCli_Node::RootNode()->execute(argc, args_mmi, 0);
 
   const char* args_mmi_screeni[] = {"mmi", "screeni"};
   argc = 2;
   printf("\nAccess invalid node\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_mmi_screeni, 1);
+  DbgCli_Node::RootNode()->execute(argc, args_mmi_screeni, 1);
 
   const char* args_branch2_subX[] = {"mmi", "sensorA", "subX"};
   argc = 3;
   printf("\nAccess middle node from other branch\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_branch2_subX, 1);
+  DbgCli_Node::RootNode()->execute(argc, args_branch2_subX, 1);
 
   const char* args_branch2_topicY[] = {"mmi", "sensorA", "subX", "topicY"};
   argc = 4;
   printf("\nAccess last node from other branch\n");
-  DbgCli_Topic::RootNode()->execute(argc, args_branch2_topicY, 1);
+  DbgCli_Node::RootNode()->execute(argc, args_branch2_topicY, 1);
 
   return 0;
 }
